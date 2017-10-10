c1627513cb2ed15cebb4ee6d1309551559a04d65
Kevin Huynh <kevinhuynh@D-10-18-245-237.dhcp4.washington.edu>
2017-10-09 17:53:35 -0700

    did exercise 2

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -17,3 +17,40 @@ too.expensive <- total.cost > 1000
 # Create a variable `max_puppies`, which is the number of puppies you can afford for $1K.  Compute this
 max_puppies <- 1000 / puppy.price
 # value in R, not just assign!
+
+convertMtoKm <- function  (mi, ft){
+  km <-1.60934 * mi + .003084 * ft
+  return(km)
+}
+
+colors <- c("blue", "red", "grey", "red", "brown")
+
+random <- rnorm(30)
+
+for(i in 1:4){
+  cat("i=", i, "\n")
+}
+
+for(x in seq(1, 20, by=2)){
+  cat(x, "\n")
+}
+employees <- c("anni", "chris", "hana", "alex")
+for(i in 1:length(employees)){
+  cat("Employee", i, "is", employees[i], "\n" )
+}
+
+for(x in seq(222, 233)){
+  cat(x, " ")
+}
+
+for(x in 1:length(state.abb)){
+  cat("State", x, "=" , state.abb[x], "\n")
+}
+
+for(x in 1:length(state.name)){
+  cat("State", x, "=" , state.name[x], "\n")
+}
+
+for(x in 1:length(state.name)){
+  cat(state.abb[x], "=" , state.name[x], "\n")
+}
